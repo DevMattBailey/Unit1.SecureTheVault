ef2842beb2ba433b774f343ff944f256f16a0416
Matthew Bailey <Matthewbailey916@gmail.com>
2025-05-14 13:56:18 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let code1;
+code1 = 7 + 3;
+console.log(code1);
+
+const code2 = 120 / 2 - 20;
+console.log(code2);
+
+const code3 = 79 % 40;
+console.log(code3);
+
+let message - 'The vault has been secured. The combination is:'
+
+const codeA = code1 + '-' + code2 + '-' + code3;
+console.log(codeA);
+
+const codeB - '${code1}-${code2)-${code3}';
+console.log(codeB);
+
+console.log(message,codeA, codeB)
+console.log(message, codeA === codeB);
